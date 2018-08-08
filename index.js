5f41d493059685fdd8912036f1357394ff62d701
Dee Race <deerace12@gmail.com>
2018-08-08 15:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 class Neighborhood {
   constructor(name) {
-    this.name = name 
+    this.name = name
     this.id = ++neighborhoodId
 
     store.neighborhoods.push(this)
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId === this.id
+    })
   }
 }
