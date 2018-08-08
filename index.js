107ffe85e184f2f90c6b963531e76a8c604cbd48
Dee Race <deerace12@gmail.com>
2018-08-08 15:13:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighborhoodId = 0
+let neighborhoodId = 0 
+let customerId = 0
+let mealId = 0
+let deliveryId = 0 
 
 
 
