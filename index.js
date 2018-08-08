00d008fe43924336ac290c804616e07bc4d57ef2
Dee Race <deerace12@gmail.com>
2018-08-08 15:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name 
+    this.id = ++neighborhoodId
+
+    store.neighborhoods.push(this)
+  }
+}
